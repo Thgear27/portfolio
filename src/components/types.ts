a67322077d9f8560f1eddc5e529da985849d6a0c
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2025-03-18 17:42:06 -0500

    add mobile menu

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,1 +1,3 @@
 export type NavbarOption = { label: string; href: string; icon?: (_props: Record<string, any>) => any };
+
+export type SocialLink = { label: string; href: string; icon: (_props: Record<string, any>) => any };
