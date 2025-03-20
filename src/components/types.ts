82d97e0651dd8146bbf0badfa6f7e4a5acc9cdff
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2025-03-19 19:41:54 -0500

    add experience section

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,3 +1,12 @@
 export type NavbarOption = { label: string; href: string; icon?: (_props: Record<string, any>) => any };
 
 export type SocialLink = { label: string; href: string; icon: (_props: Record<string, any>) => any };
+
+export type JobInfo = {
+  title: string;
+  company: string;
+  date: string;
+  location: string;
+  description: string;
+  workTeamates?: { name: string; imgURL: string }[];
+};
